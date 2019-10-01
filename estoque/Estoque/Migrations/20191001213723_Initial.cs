0a1be769b213db187b640279b038967cac2866c2
Aluno <Aluno@DOSO122759.educacional.up>
2019-10-01 18:39:35 -0300

    Added base migration to estoque

---
+++ estoque/Estoque/Migrations/20191001213723_Initial.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Estoque.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Deposito",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DataHora = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deposito", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Documento",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Tipo = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventario",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    produtoId = table.Column<Guid>(nullable: false),
+                    quantidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventario", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filial",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DepositoId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filial", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Filial_Deposito_DepositoId",
+                        column: x => x.DepositoId,
+                        principalTable: "Deposito",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false),
+                    Valor = table.Column<double>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Tipo = table.Column<string>(nullable: true),
+                    DepositoId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Deposito_DepositoId",
+                        column: x => x.DepositoId,
+                        principalTable: "Deposito",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movimento",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Data = table.Column<DateTime>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false),
+                    ProdutoId = table.Column<Guid>(nullable: false),
+                    Natureza = table.Column<int>(nullable: false),
+                    DocumentoId = table.Column<Guid>(nullable: true),
+                    Tipo = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movimento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movimento_Documento_DocumentoId",
+                        column: x => x.DocumentoId,
+                        principalTable: "Documento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Movimento_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Filial_DepositoId",
+                table: "Filial",
+                column: "DepositoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimento_DocumentoId",
+                table: "Movimento",
+                column: "DocumentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimento_ProdutoId",
+                table: "Movimento",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_DepositoId",
+                table: "Produtos",
+                column: "DepositoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filial");
+
+            migrationBuilder.DropTable(
+                name: "Inventario");
+
+            migrationBuilder.DropTable(
+                name: "Movimento");
+
+            migrationBuilder.DropTable(
+                name: "Documento");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Deposito");
+        }
+    }
+}
