a63195f7dbce57eef4df6d5885b623a27d470be3
Aluno <Aluno@DOSO122759.educacional.up>
2019-10-01 18:40:53 -0300

    Added Initial migration to escobar-backend

--- escobar-backend/IdentityServer4AngularOIDC/Migrations/20191001214008_Initial.cs
+++ escobar-backend/IdentityServer4AngularOIDC/Migrations/20191001214008_Initial.cs
@@ -8,17 +8,45 @@ namespace Server.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
+                name: "Comandos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Modulo = table.Column<string>(nullable: true),
+                    ComandoName = table.Column<string>(nullable: true),
+                    Sistema = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comandos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "UserAccesses",
                 columns: table => new
                 {
                     ID = table.Column<Guid>(nullable: false),
                     DataAccess = table.Column<DateTime>(nullable: false),
                     Success = table.Column<bool>(nullable: false),
-                    UserID = table.Column<Guid>(nullable: false)
+                    UserID = table.Column<Guid>(nullable: false),
+                    Log = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_UserAccesses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserComandos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    ComandoId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserComandos", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -42,7 +70,13 @@ namespace Server.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
+                name: "Comandos");
+
+            migrationBuilder.DropTable(
                 name: "UserAccesses");
+
+            migrationBuilder.DropTable(
+                name: "UserComandos");
 
             migrationBuilder.DropTable(
                 name: "Users");
